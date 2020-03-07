1ce4fd2db0d7e822d03ef4c1eb8394630e59a060
langhuihui <178529795@qq.com>
2020-03-07 14:16:38 +0800

    转换为子模块方式

--- monica/util/bits/pio/pio.go
+++
@@ -1,3 +0,0 @@
-package pio
-
-var RecommendBufioSize = 1024 * 64
--- pm/src/plugins/iview.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import ViewUI from 'view-design'
-
-Vue.use(ViewUI)
-
-import 'view-design/dist/styles/iview.css'
