306a7ffeff0504636a4f84dcd59912903ef9b6dd
dexter <178529795@qq.com>
2022-04-08 16:07:00 +0800

    加入WebTransport插件

--- main.go
+++ main.go
@@ -12,8 +12,10 @@ import (
 	_ "m7s.live/plugin/hls/v4"
 	_ "m7s.live/plugin/jessica/v4"
 	_ "m7s.live/plugin/logrotate/v4"
+	// _ "m7s.live/plugin/record/v4"
 	_ "m7s.live/plugin/rtmp/v4"
 	_ "m7s.live/plugin/rtsp/v4"
+	_ "m7s.live/plugin/webtransport/v4"
 )
 
 func main() {
