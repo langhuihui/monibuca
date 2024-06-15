c8553f207316403a4be1ce77a71a5662f6fc03ee
sean.zhang <sean.zhang@digitwin.com>
2024-06-15 10:53:26 +0800

    支持环境变量指定配置文件路径

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 //go:generate go run gen.go $debug
 
-/* 
-███    ███  ██████  ███    ██ 👑 ██████  ██    ██  ██████  █████ 
+/*
+███    ███  ██████  ███    ██ 👑 ██████  ██    ██  ██████  █████
 ████  ████ ██    ██ ████   ██ ██ ██   ██ ██    ██ ██      ██   ██
 ██ ████ ██ ██    ██ ██ ██  ██ ██ ██████  ██    ██ ██      ███████
 ██  ██  ██ ██    ██ ██  ██ ██ ██ ██   ██ ██    ██ ██      ██   ██
@@ -36,17 +36,20 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"os"
 
 	"m7s.live/engine/v4"
 	"m7s.live/engine/v4/util"
 
-	_ "m7s.live/plugin/logrotate/v4"
 	_ "m7s.live/plugin/debug/v4"
+	_ "m7s.live/plugin/fmp4/v4"
 	_ "m7s.live/plugin/gb28181/v4"
 	_ "m7s.live/plugin/hdl/v4"
 	_ "m7s.live/plugin/hls/v4"
 	_ "m7s.live/plugin/hook/v4"
 	_ "m7s.live/plugin/jessica/v4"
+	_ "m7s.live/plugin/logrotate/v4"
+	_ "m7s.live/plugin/monitor/v4"
 	_ "m7s.live/plugin/preview/v4"
 	_ "m7s.live/plugin/record/v4"
 	_ "m7s.live/plugin/room/v4"
@@ -55,8 +58,6 @@ import (
 	_ "m7s.live/plugin/snap/v4"
 	_ "m7s.live/plugin/webrtc/v4"
 	_ "m7s.live/plugin/webtransport/v4"
-	_ "m7s.live/plugin/fmp4/v4"
-	_ "m7s.live/plugin/monitor/v4"
 )
 
 var (
@@ -65,7 +66,12 @@ var (
 
 func main() {
 	fmt.Println("start github.com/langhuihui/monibuca version:", version)
-	conf := flag.String("c", "config.yaml", "config file")
+	confPathFromEnv := os.Getenv("BUCA_CONFIG_FILE")
+	if confPathFromEnv == "" {
+		confPathFromEnv = "config.yaml" // 如果环境变量未设置，默认使用此路径
+	}
+	fmt.Println("=== config file: ", confPathFromEnv)
+	conf := flag.String("c", confPathFromEnv, "config file")
 	flag.Parse()
 	ctx, cancel := context.WithCancel(context.WithValue(context.Background(), "version", version))
 	go util.WaitTerm(cancel)
