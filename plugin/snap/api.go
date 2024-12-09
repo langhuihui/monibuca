3b0f38f468a827586b9d5e411de8c42991d64a75
banshan <dwdcth@163.com>
2024-12-09 23:26:58 +0800

    fix:  incomplete part of the snap image

--- plugin/snap/api.go
+++ plugin/snap/api.go
@@ -1,8 +1,9 @@
 package plugin_snap
 
 import (
-	"io"
+	"bytes"
 	"net/http"
+	"strconv"
 
 	"m7s.live/v5/pkg"
 	"m7s.live/v5/pkg/config"
@@ -13,66 +14,34 @@ func (t *SnapPlugin) doSnap(rw http.ResponseWriter, r *http.Request) {
 	streamPath := r.PathValue("streamPath")
 	targetStreamPath := streamPath
 
-	ok := t.Server.Streams.Has(streamPath)
-	if !ok {
+	if !t.Server.Streams.Has(streamPath) {
 		http.Error(rw, pkg.ErrNotFound.Error(), http.StatusNotFound)
 		return
 	}
 
-	pr, pw := io.Pipe()
+	buf := new(bytes.Buffer)
+	transformer := snap.NewTransform().(*snap.Transformer)
+	transformer.TransformJob.Init(transformer, &t.Plugin, streamPath, config.Transform{
+		Output: []config.TransfromOutput{
+			{
+				Target:     targetStreamPath,
+				StreamPath: targetStreamPath,
+				Conf:       buf,
+			},
+		},
+	}).WaitStarted()
 
-	flusher, ok := rw.(http.Flusher)
-	if !ok {
-		http.Error(rw, "Streaming unsupported", http.StatusInternalServerError)
+	transformer.TriggerSnap()
+	if err := transformer.Run(); err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	rw.Header().Set("Content-Type", "image/jpeg")
-	rw.Header().Set("Transfer-Encoding", "chunked")
-	rw.Header().Del("Content-Length")
+	rw.Header().Set("Content-Length", strconv.Itoa(buf.Len()))
 
-	done := make(chan error, 1)
-
-	go func() {
-		defer pw.Close()
-		defer pr.Close()
-		defer close(done)
-
-		buf := make([]byte, 32*1024)
-		_, err := io.CopyBuffer(rw, pr, buf)
-		if err != nil {
-			t.Error("write response error", err)
-			done <- err
-			return
-		}
-		flusher.Flush()
-		done <- nil
-	}()
-
-	var transformer *snap.Transformer
-	if tm, ok := t.Server.Transforms.Get(targetStreamPath); ok {
-		transformer, ok = tm.TransformJob.Transformer.(*snap.Transformer)
-		if !ok {
-			http.Error(rw, "not a snap transformer", http.StatusInternalServerError)
-			return
-		}
-	} else {
-		transformer = snap.NewTransform().(*snap.Transformer)
-		transformer.TransformJob.Init(transformer, &t.Plugin, streamPath, config.Transform{
-			Output: []config.TransfromOutput{
-				{
-					Target:     targetStreamPath,
-					StreamPath: targetStreamPath,
-					Conf:       pw,
-				},
-			},
-		}).WaitStarted()
-	}
-
-	transformer.TriggerSnap()
-
-	if err := <-done; err != nil {
-		t.Error("snapshot failed", err)
+	if _, err := buf.WriteTo(rw); err != nil {
+		t.Error("write response error", err)
 		return
 	}
 }
